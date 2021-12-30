3b866a1ce0b77aac97b5056113c649619a5b622f
Deyvid Gondim de Jesus <86028187+DeyvidJesus@users.noreply.github.com>
2021-12-30 17:39:32 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const botao = document.getElementById('botao-mobile')
-const menu = document.getElementById('header-nav')
-
-botao.addEventListener('click', function(){
-    menu.classList.toggle('active')
-    botao.classList.toggle('active')
-})
